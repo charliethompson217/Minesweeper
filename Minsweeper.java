81404ab4146c3bae5933a2d48542e4a5b38bd54c
charliethompson217 <80548854+charliethompson217@users.noreply.github.com>
2023-05-14 15:51:13 -0400

    Update Minsweeper.java

--- Minsweeper.java
+++ Minsweeper.java
@@ -11,82 +11,67 @@ public class Minsweeper extends PApplet {
 	int cellSize = 30;
 	double dificulty = 1.5;
 
-	// seed for random number generator
-	long seed = System.currentTimeMillis();
-
 	// globals
 	boolean lost = false;
 	boolean won = false;
 	Cell cells[][] = new Cell[width][height];
 	PImage mine;
 	PImage flag;
+	PImage smiley;
+	ScoreBoard scoreBoard;
 	int mines = (int) ((width * height * dificulty) / 10);
+
+	int offX = 0;
+	int offY = 50;
 
 	public static void main(String[] args) {
 		PApplet.main("Minsweeper");
-
 	}
 
 	public void settings() {
-		this.size(cellSize * width, cellSize * height);
+		this.size(cellSize * width + offX, cellSize * height + offY);
 	}
 
 	public void setup() {
 		background(255);
 		mine = loadImage("mine.png");
-		flag = loadImage("hammer-sickle.png");
-		// initialize cells
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				cells[i][j] = new Cell(i, j);
-			}
-		}
-		// place bombs
-		Random random = new Random(seed);
-		for (int i = 0; i < mines; i++) {
-			int x = (int) (random.nextDouble() * width);
-			int y = (int) (random.nextDouble() * height);
-			Cell cell = cells[x][y];
-			if (cell.isBomb) {
-				i--;
-				continue;
-			}
-			cell.placeBomb();
-		}
-		// count totals
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				cells[i][j].count();
-			}
-		}
+		flag = loadImage("flag.png");
+		smiley = loadImage("smiley.png");
+		startGame();
 	}
 
 	public void draw() {
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				cells[i][j].update();
+				cells[i][j].update(offX, offY);
 			}
 		}
 		if (lost) {
 			textSize(cellSize);
 			fill(250, 0, 0);
-			text("Game Over", (float) (((width * cellSize) / 2) - cellSize * 2.5), (height * cellSize) / 2);
+			text("Game Over", (float) (((width * cellSize) / 2) - cellSize * 2.5) + offX,
+					(height * cellSize) / 2 + offY);
 		}
 		if (won) {
 			textSize(cellSize);
 			fill(250, 0, 0);
-			text("Player Won!", (float) (((width * cellSize) / 2) - cellSize * 2.5), (height * cellSize) / 2);
+			text("Player Won!", (float) (((width * cellSize) / 2) - cellSize * 2.5) + offX,
+					(height * cellSize) / 2 + offY);
 		}
+		scoreBoard.update();
 	}
 
 	public void mouseClicked() {
+		if (mouseY < offY) {
+			if (mouseX > (width * cellSize) / 2 - offY && mouseX < (width * cellSize) / 2 + offY)
+				startGame();
+			return;
+		}
 		if (lost)
 			return;
-
-		int X = mouseX;
-		int Y = mouseY;
+		int X = mouseX - offX;
+		int Y = mouseY - offY;
 		Cell cell = cells[X / cellSize][Y / cellSize];
-
 		if (cell.isOpen)
 			return;
 
@@ -115,11 +100,11 @@ public class Minsweeper extends PApplet {
 			this.y = y;
 		}
 
-		void update() {
+		void update(int offX, int offY) {
 			fill(r, g, b);
 			stroke(0, 0, 0);
 			strokeWeight(0);
-			square(x * cellSize, y * cellSize, cellSize);
+			square(x * cellSize + offX, y * cellSize + offY, cellSize);
 			fill(0, 0, 0);
 			if (isOpen && count > 0) {
 				textSize((float) (cellSize));
@@ -150,29 +135,34 @@ public class Minsweeper extends PApplet {
 					break;
 
 				}
-				text(Integer.toString(count), x * cellSize + (3 * cellSize / 12), y * cellSize + (4 * cellSize / 5));
+				text(Integer.toString(count), x * cellSize + (3 * cellSize / 12) + offX,
+						y * cellSize + (4 * cellSize / 5) + offY);
 				if (isBomb) {
 					image(mine, x * cellSize, y * cellSize, cellSize, cellSize);
 				}
 			}
 			if (isFlaged) {
-				image(flag, x * cellSize + 2, y * cellSize + 2, cellSize - 4, cellSize - 4);
+				image(flag, x * cellSize + 2 + offX, y * cellSize + 2 + offY, cellSize - 4, cellSize - 4);
 			}
 			if (lost) {
 				if (isBomb && !isFlaged) {
-					image(mine, x * cellSize, y * cellSize, cellSize, cellSize);
+					image(mine, x * cellSize + offX, y * cellSize + offY, cellSize, cellSize);
 				}
 				if (isBomb && isOpen) {
 					stroke(250, 0, 0);
 					strokeWeight(2);
-					line(x * cellSize, y * cellSize, x * cellSize + cellSize, y * cellSize + cellSize);
-					line(x * cellSize + cellSize, y * cellSize, x * cellSize, y * cellSize + cellSize);
+					line(x * cellSize + offX, y * cellSize + offY, x * cellSize + cellSize + offX,
+							y * cellSize + cellSize + offY);
+					line(x * cellSize + cellSize + offX, y * cellSize + offY, x * cellSize + offX,
+							y * cellSize + cellSize + offY);
 				}
 				if (!isBomb && isFlaged) {
 					stroke(0, 0, 0);
 					strokeWeight(2);
-					line(x * cellSize, y * cellSize, x * cellSize + cellSize, y * cellSize + cellSize);
-					line(x * cellSize + cellSize, y * cellSize, x * cellSize, y * cellSize + cellSize);
+					line(x * cellSize + offX, y * cellSize + offY, x * cellSize + cellSize + offX,
+							y * cellSize + cellSize + offY);
+					line(x * cellSize + cellSize + offX, y * cellSize + offY, x * cellSize + offX,
+							y * cellSize + cellSize + offY);
 				}
 
 			}
@@ -260,5 +250,52 @@ public class Minsweeper extends PApplet {
 
 	void playerWon() {
 		won = true;
+	}
+
+	class ScoreBoard {
+		int size;
+
+		ScoreBoard(int size) {
+			this.size = size;
+		}
+
+		void update() {
+			fill(250, 0, 0);
+			square(((width * cellSize) / 2) - size / 2 + offX, 0, size);
+			image(smiley, ((width * cellSize) / 2) - size / 2 + offX, 0, size, size);
+		}
+	}
+
+	void startGame() {
+		lost = false;
+		won = false;
+		cells = new Cell[width][height];
+		scoreBoard = new ScoreBoard(offY);
+		// seed for random number generator
+		long seed = System.currentTimeMillis();
+		// initialize cells
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				cells[i][j] = new Cell(i, j);
+			}
+		}
+		// place bombs
+		Random random = new Random(seed);
+		for (int i = 0; i < mines; i++) {
+			int x = (int) (random.nextDouble() * width);
+			int y = (int) (random.nextDouble() * height);
+			Cell cell = cells[x][y];
+			if (cell.isBomb) {
+				i--;
+				continue;
+			}
+			cell.placeBomb();
+		}
+		// count totals
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				cells[i][j].count();
+			}
+		}
 	}
 }
